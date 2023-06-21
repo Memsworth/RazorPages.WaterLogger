35dfcea7cda85e02723a9bffbe05a3771553db1a
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-06-22 07:15:56 +0900

    Update Service

--- WaterLogger.Domain/Abstraction/Services/IWaterService.cs
+++ WaterLogger.Domain/Abstraction/Services/IWaterService.cs
@@ -5,7 +5,7 @@ namespace WaterLogger.Domain.Abstraction.Services;
 public interface IWaterService
 {
     Task<Water> GetExerciseByIdAsync(int id);
-    Task<IQueryable<Water>> GetAllExerciseAsync();
+    Task<IEnumerable<Water>> GetAllExerciseAsync();
     Task AddExerciseAsync(Water waterItem);
     Task UpdateExerciseAsync(Water waterItem, Water waterUpdate);
     Task DeleteExerciseAsync(Water waterItem);
---
+++ WaterLogger.Service/WaterService.cs
@@ -0,0 +1,33 @@
+using WaterLogger.Domain.Abstraction.Services;
+using WaterLogger.Domain.Abstraction.UnitOfWork;
+using WaterLogger.Domain.Models;
+
+namespace WaterLogger.Service;
+
+public class WaterService : IWaterService
+{
+    protected readonly IUnitOfWork _unitOfWork;
+    public WaterService(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+
+    public async Task<Water> GetExerciseByIdAsync(int id) => await _unitOfWork.WaterLoggerRepository.GetAsync(id);
+
+    public async Task<IEnumerable<Water>> GetAllExerciseAsync() => await _unitOfWork.WaterLoggerRepository.GetAsync();
+
+    public async Task AddExerciseAsync(Water waterItem)
+    {
+        await _unitOfWork.WaterLoggerRepository.AddAsync(waterItem);
+        await _unitOfWork.CommitAsync();
+    }
+
+    public async Task UpdateExerciseAsync(Water waterItem, Water waterUpdate)
+    {
+        await _unitOfWork.CommitAsync();
+    }
+
+    public async Task DeleteExerciseAsync(Water waterItem)
+    {
+        _unitOfWork.WaterLoggerRepository.Delete(waterItem);
+        await _unitOfWork.CommitAsync();
+    }
+}
